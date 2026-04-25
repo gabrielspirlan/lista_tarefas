dba0155abb2a894b606cb81478b966ba2f3735fa
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2026-04-25 19:44:39 -0300

    fix: Add validações nos campos da entidade

--- src/main/java/com/api/lista_tarefas/domain/entities/Tarefa.java
+++ src/main/java/com/api/lista_tarefas/domain/entities/Tarefa.java
@@ -1,16 +1,17 @@
 package com.api.lista_tarefas.domain.entities;
 
+import com.api.lista_tarefas.domain.enums.TarefaStatusConverter;
 import com.api.lista_tarefas.domain.enums.TarefaStatusEnum;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -21,11 +22,34 @@ public class Tarefa {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotBlank
     private String descricao;
+
+    @NotNull
+    @Convert(converter = TarefaStatusConverter.class)
     private TarefaStatusEnum status;
+
+    @Nullable
     private String observacoes;
-    private Date dataCriacao;
-    private Date dataAtualizacao;
+
+    @Column(name = "data_criacao", updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @PrePersist
+    public void prePersist() {
+        dataCriacao = LocalDateTime.now();
+        dataAtualizacao = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = LocalDateTime.now();
+    }
+
 }
