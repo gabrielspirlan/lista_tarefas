7e5b41aaec7e925a26da76b36098c46a675e4768
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2026-04-26 18:38:16 -0300

    feature: script para criar o banco de dados do projeto

---
+++ criar-banco.sql
@@ -0,0 +1,20 @@
+-- Script de criação da tabela Tarefas
+-- Banco de Dados: PostgreSQL
+
+CREATE DATABASE tarefas;
+
+CREATE TABLE IF NOT EXISTS tarefas (
+    id SERIAL PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL,
+    status VARCHAR(3) NOT NULL,
+    observacoes TEXT,
+    data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    -- Constraint para garantir que apenas valores válidos do Enum sejam inseridos
+    -- PE  = PENDENTE
+    -- AND = EM_ANDAMENTO
+    -- CON = CONCLUIDA
+    CONSTRAINT chk_status CHECK (status IN ('PE', 'AND', 'CON'))
+);
+
